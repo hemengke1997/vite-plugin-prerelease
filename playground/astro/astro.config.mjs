963f5c3cb0561063ddc4890e0b6632bb1e8ea35f
hemengke <23536175@qq.com>
2025-02-17 16:37:47 +0800

    update: typo

--- playground/astro/astro.config.mjs
+++ playground/astro/astro.config.mjs
@@ -5,19 +5,9 @@ import { prerelease } from 'vite-plugin-prerelease/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react({
-      // exclude: [/vite-plugin-prerelease.*\.js/],
-    }),
-    prerelease(),
-  ],
+  integrations: [react(), prerelease()],
   output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
-  vite: {
-    build: {
-      minify: false,
-    },
-  },
 })
